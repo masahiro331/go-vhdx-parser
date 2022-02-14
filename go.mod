5ce85ed0a2698df4a314e4c76fbb1b54bdad7e2a
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-02-15 01:10:01 +0900

    feat(main): wip parse vhdx

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/masahiro331/go-vhdx-parser
 
 go 1.17
 
-require golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+require (
+	github.com/google/uuid v1.3.0
+	github.com/masahiro331/go-vmdk-parser v0.0.0-20211230085158-c846df893199
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+)
 
-require github.com/beevik/guid v0.0.0-20170504223318-d0ea8faecee0 // indirect
+replace github.com/masahiro331/go-ext4-filesystem => ../go-ext4-filesystem
--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
+	"github.com/masahiro331/go-vhdx-parser/pkg/vhdk"
+	"github.com/masahiro331/go-vmdk-parser/pkg/disk"
 	"io"
 	"log"
 	"os"
-
-	"golang.org/x/xerrors"
-)
-
-const (
-	BitmapAllocationGroup = "2DC27766-F623-4200-9D64-115E9BFD4A08"
-	MetadataRegion        = "8B7CA206-4790-4B9A-B8FE-575F050F886E"
-	Code                  = 0x1EDC6F41
 )
 
 func main() {
@@ -22,140 +14,36 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	v := VHDX{}
-	v.HeaderSection, err = parseHeaderSection(f)
+	_, err = vhdk.NewVHDX(f)
 	if err != nil {
-		log.Fatalf("%+v\n", err)
+		log.Fatal(err)
 	}
-
-}
-
-type VHDX struct {
-	HeaderSection HeaderSection // 1MB Align
-}
-
-const (
-	_64KB = int(64 << 10)
-	_1MB  = int(1 << 20)
-)
-
-func parseHeaderSection(rs io.ReadSeeker) (HeaderSection, error) {
-	var hs HeaderSection
-	var err error
-	if err := binaryRead(rs, binary.LittleEndian, &hs.FileIdentifer, _64KB); err != nil {
-		return HeaderSection{}, xerrors.Errorf("failed to parse file identifier: %w", err)
-	}
-	if err := binaryRead(rs, binary.LittleEndian, &hs.Header1, _64KB); err != nil {
-		return HeaderSection{}, xerrors.Errorf("failed to parse header 1: %w", err)
-	}
-	if err := binaryRead(rs, binary.LittleEndian, &hs.Header2, _64KB); err != nil {
-		return HeaderSection{}, xerrors.Errorf("failed to parse header 2: %w", err)
-	}
-	hs.RegionTable1, err = parseRegionTable(rs)
+	of, err := os.Open("output.file")
 	if err != nil {
-		return HeaderSection{}, xerrors.Errorf("failed to parse region table 1: %w", err)
+		log.Fatal(err)
 	}
-	hs.RegionTable2, err = parseRegionTable(rs)
+
+	driver, err := disk.NewDriver(of)
 	if err != nil {
-		return HeaderSection{}, xerrors.Errorf("failed to parse region table 2: %w", err)
+		log.Fatal(err)
 	}
 
-	return hs, nil
-}
-
-func parseRegionTable(rs io.ReadSeeker) (RegionTable, error) {
-	buf := make([]byte, _64KB)
-	n, err := rs.Read(buf)
-	if err != nil {
-		return RegionTable{}, xerrors.Errorf("failed to read buf: %w", err)
-	}
-	if n != _64KB {
-		return RegionTable{}, xerrors.Errorf("read length error: %d", n)
-	}
-
-	r := bytes.NewReader(buf)
-	var rt RegionTable
-	if err := binary.Read(r, binary.LittleEndian, &rt.RegionTableHeader); err != nil {
-		return RegionTable{}, xerrors.Errorf("failed to read region table header: %w", err)
-	}
-	for n := uint32(0); n < rt.RegionTableHeader.EntryCount; n++ {
-		var entry RegionTableEntry
-		if err := binary.Read(r, binary.LittleEndian, &entry); err != nil {
-			return RegionTable{}, xerrors.Errorf("failed to read region table entry %d: %w", n, err)
+	for i, p := range driver.GetPartitions() {
+		pf, err := os.Create(fmt.Sprintf("%s%d.img", p.Name(), i))
+		if err != nil {
+			log.Fatal(err)
 		}
-		rt.RegionTableEntries = append(rt.RegionTableEntries, entry)
+
+		_, err = of.Seek(int64(p.GetStartSector())*512, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		reader := io.LimitReader(of, int64(p.GetSize())*512)
+		i, err := io.Copy(pf, reader)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("file size:", i)
 	}
-
-	return rt, nil
-}
-
-func binaryRead(r io.Reader, o binary.ByteOrder, v interface{}, align int) error {
-	buf := make([]byte, align)
-	n, err := r.Read(buf)
-	if err != nil {
-		return xerrors.Errorf("failed to read buf: %w", err)
-	}
-	if n != align {
-		return xerrors.Errorf("read length error: %d", n)
-	}
-
-	if err := binary.Read(bytes.NewReader(buf), o, v); err != nil {
-		return xerrors.Errorf("failed to read binary: %w", err)
-	}
-
-	return nil
-}
-
-type HeaderSection struct {
-	FileIdentifer FileIdentifer // 64KB Align
-	Header1       Header        // 64KB Align
-	Header2       Header        // 64KB Align
-	RegionTable1  RegionTable   // 64KB Align
-	RegionTable2  RegionTable   // 64KB Align
-}
-
-type GUID [16]byte
-
-func (g GUID) String() string {
-	return fmt.Sprintf("%x-%x-%x-%x-%x",
-		g[0:4], g[4:6], g[6:8], g[8:10], g[10:16])
-}
-
-type FileIdentifer struct {
-	Signature [8]byte
-	Creator   [512]byte
-}
-
-type Header struct {
-	Signature      [4]byte
-	Checksum       [4]byte
-	SequenceNumber uint64
-	FileWriteGUID  GUID
-	DataWriteGUID  GUID
-	LogGUID        GUID
-	LogVersion     uint16
-	Version        uint16
-	LogLength      uint32
-	LogOffset      uint64
-	_              [4016]byte
-}
-
-type RegionTable struct {
-	RegionTableHeader  RegionTableHeader
-	RegionTableEntries []RegionTableEntry
-}
-
-type RegionTableHeader struct {
-	Signature  [4]byte
-	Checksum   [4]byte
-	EntryCount uint32
-	Required   [4]byte
-}
-
-type RegionTableEntry struct {
-	GUID       GUID
-	FileOffset uint64
-	Length     uint32
-	Required   [4]byte
 }
