a8420b519e1d07354e14775eb10376b4a5755fec
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-02-23 14:24:02 +0900

    feat(vhdx): parse ext4

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/masahiro331/go-vhdx-parser
 go 1.17
 
 require (
-	github.com/google/uuid v1.3.0
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+	github.com/masahiro331/go-disk v0.0.0-20220220182714-f5f832728b22 // indirect
+	github.com/masahiro331/go-ext4-filesystem v0.0.0-20220223051406-4de5dcd813f3 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
-
-require github.com/masahiro331/go-disk v0.0.0-20220220145841-bf888849647c
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"github.com/masahiro331/go-disk"
-	"github.com/masahiro331/go-vhdx-parser/pkg/vhdx"
+	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+
+	disk "github.com/masahiro331/go-disk"
+	"github.com/masahiro331/go-ext4-filesystem/ext4"
+	"github.com/masahiro331/go-vhdx-parser/pkg/vhdx"
+	"golang.org/x/xerrors"
 )
 
 func main() {
@@ -18,22 +23,45 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.Create("primary.ext4")
-	if err != nil {
-		log.Fatal(err)
-	}
+
 	for {
 		p, err := driver.Next()
 		if err != nil {
 			log.Fatal(err)
 		}
-		if !p.Bootable() {
-			for {
-				_, err := io.CopyN(f, p, 512)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
+		if p.Bootable() {
+			continue
 		}
+
+		filesystem, err := ext4.NewFS(p.GetSectionReader())
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = fs.WalkDir(filesystem, "/", func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return xerrors.Errorf("file walk error: %w", err)
+			}
+			if d.IsDir() {
+				return nil
+			}
+
+			fmt.Println(path)
+			if path == "/usr/lib/os-release" {
+				of, _ := os.Create("os-release")
+				defer of.Close()
+
+				sf, err := filesystem.Open(path)
+				if err != nil {
+					return err
+				}
+				io.Copy(of, sf)
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatalf("%+v\n", err)
+		}
+
+		return
 	}
 }
